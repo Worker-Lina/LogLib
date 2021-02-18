c7585e858b242795da0e985f6478a8f66c517bf1
алина человечкова <alinasmart19@mail.ru>
2021-02-18 20:23:42 +0600

    Добавьте файлы проекта.

---
+++ LogLibrary/Program.cs
@@ -0,0 +1,27 @@
+﻿using Logging;
+using Newtonsoft.Json;
+using System;
+
+namespace LogLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var log = new Log();
+            try
+            {
+                string pathToLog = $"{DateTime.Now.ToString("d")}.log";
+                Console.WriteLine(pathToLog);
+                int n = int.Parse(Console.ReadLine());
+            }
+            catch(Exception ex)
+            {                
+                log.Logger(ex);
+            }
+
+            string jsonTextResult = JsonConvert.SerializeObject(log);
+
+        }
+    }
+}
---
+++ Logging/Log.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Logging
+{
+    public class Log
+    {
+        public string Message { get; set; }
+        public Type TypeMessage { get; set; }
+        public string Level { get; set; }
+
+
+        private string PathToLog { get; set; }
+
+
+        public void Logger(Exception ex)
+        {
+            Message = ex.Message;
+            PathToLog = $"{DateTime.Now.ToString("d")}.log";
+
+            using (StreamWriter streamWriter = new StreamWriter(PathToLog, true, System.Text.Encoding.Default))
+            {
+                string logInformation = $"{DateTime.Now.ToString("G")}, {ex.TargetSite.DeclaringType}, {ex.TargetSite.Name}, {ex.Message}";
+                streamWriter.WriteLine(logInformation);
+            }
+        }
+
+        public void Logger(string message, Exception ex)
+        {
+            Message = message;
+            PathToLog = $"{DateTime.Now.ToString("d")}.log";
+
+            using (StreamWriter streamWriter = new StreamWriter(PathToLog, true, System.Text.Encoding.Default))
+            {
+                string logInformation = $"{DateTime.Now.ToString("G")}, {ex.TargetSite.DeclaringType}, {ex.TargetSite.Name}, {message}";
+                streamWriter.WriteLine(logInformation);
+            }
+        }
+
+        public void Info()
+        {
+            PathToLog = $"{DateTime.Now.ToString("d")}.log";
+
+            using (StreamWriter streamWriter = new StreamWriter(PathToLog, true, System.Text.Encoding.Default))
+            {
+                string logInformation = $"{DateTime.Now.ToString("G")}, {Message}";
+                streamWriter.WriteLine(logInformation);
+            }
+        }
+    }
+}
